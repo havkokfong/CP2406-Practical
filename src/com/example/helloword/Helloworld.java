baddd72aa9a4adc366a25d600e91086fac8664ce
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-03-20 14:47:32 +0800

    Edited

--- src/com/example/helloword/Helloworld.java
+++ src/com/example/helloword/Helloworld.java
@@ -3,5 +3,6 @@ package com.example.helloword;
 public class Helloworld {
     public static void main(String[] args){
         System.out.println("Hello Word!");
+        System.out.println("Edited");
     }
 }
