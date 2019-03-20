248175215ab1b709013612cc84d9900926e42544
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-03-20 17:56:45 +0800

    Edited

--- Practical 1/src/MovieQuoteInfo.java
+++ Practical 1/src/MovieQuoteInfo.java
@@ -1,5 +1,5 @@
 public class MovieQuoteInfo {
-    public static void main(String[] agrs){
+    public static void main(String[] args){
             System.out.println("Transporter 3");
         }
 }
