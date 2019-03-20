276fec0229f06ee1315d7467b36e32720652dcf5
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-03-20 18:06:50 +0800

    Edited

--- Practical 1/src/MovieQuoteInfo.java
+++ Practical 1/src/MovieQuoteInfo.java
@@ -1,5 +1,6 @@
 public class MovieQuoteInfo {
     public static void main(String[] args){
-            System.out.println("Transporter 3");
+            System.out.println("I want my morning to be quiet");
+            System.out.println("The Transporter, Jason Statham, 2002");
         }
 }
