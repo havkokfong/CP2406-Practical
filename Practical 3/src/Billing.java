21a2d4099ec8ecbd4b87770a590d2302eda1fb46
De-ToNyS <havkokfong@my.jcu.edu.au>
2019-04-05 03:13:14 +0800

    new

---
+++ Practical 3/src/Billing.java
@@ -0,0 +1,24 @@
+import javax.swing.text.Style;
+import java.util.Scanner;
+
+public class Billing {
+    private static int bill;
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        bill = input.nextInt();
+        computingBill(bill);
+
+    }
+
+    public static int computingBill(int A){
+
+    }
+
+    public static int computingBill(int A, int B){
+
+    }
+
+    public static int computingBill(int A, int B, int C){
+
+    }
+}
