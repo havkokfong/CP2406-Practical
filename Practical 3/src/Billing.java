4cfe82cb8c3c13e9273ec14950b913c1404135c9
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-04-05 17:39:58 +0800

    Finished

--- Practical 3/src/Billing.java
+++ Practical 3/src/Billing.java
@@ -1,24 +1,44 @@
 import javax.swing.text.Style;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Billing {
     private static int bill;
+    private static int value;
+    private static double price;
+    private static double total;
+    private static double tax;
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of your bill: ");
         bill = input.nextInt();
-        computingBill(bill);
+        value = 0;
+        ArrayList<Double> billlist = new ArrayList<Double>();
+        while (value < bill){
+            System.out.println("Please enter the price of your item: ");
+            price = input.nextInt();
+            value += 1;
+            billlist.add(price);
+        }
+        if (bill == 1)
+        computingBill(billlist.get(0));
+        computingBill(billlist.get(0), billlist.get(1));
+        computingBill(billlist.get(0), billlist.get(1), billlist.get(2));
 
     }
 
-    public static int computingBill(int A){
+    public static double computingBill(double A){
+        tax = (A * 8) / 100;
+        total = A + tax;
+        System.out.println("The total bill is: " + total);
+        return total;
+    }
+
+    public static int computingBill(double A, double B){
 
     }
 
-    public static int computingBill(int A, int B){
+    public static int computingBill(double A, double B, double C){
 
-    }
-
-    public static int computingBill(int A, int B, int C){
-
-    }
+    }*/
 }
